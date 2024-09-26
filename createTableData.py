541d16836776249b29e2916f46d0c864a05d3eb5
joyiwayan <juliantapradnyana@gmail.com>
2024-09-26 10:05:31 +0700

    Database Update

--- createTableData.py
+++ createTableData.py
@@ -1,22 +1,27 @@
 import mysql.connector
 
-# connect to DB
+# Connect to DB
 db = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  passwd="",
-  database="data_pendamping_halal"
+    host="localhost",
+    user="root",
+    passwd="",
+    database="data_pendamping_halal"
 )
 cursor = db.cursor()
 
-# create table
+# Create table with updated fields
 sql = """CREATE TABLE data_pph (
    id INT AUTO_INCREMENT PRIMARY KEY,
    email VARCHAR(255),
    name VARCHAR(255),
    no_telp VARCHAR(255),
+   kabupaten VARCHAR(255),
+   kecamatan VARCHAR(255),
+   no_registrasi VARCHAR(20),
+   tgl_terbit DATE,
+   lembaga VARCHAR(255),
    pendampingan_pelaku_usaha TEXT
 )"""
 cursor.execute(sql)
 
-print('table data_pph created')
+print('Table data_pph created')
--- createTableNewData.py
+++ createTableNewData.py
@@ -9,15 +9,20 @@ db = mysql.connector.connect(
 )
 cursor = db.cursor()
 
-# create table
+# create table with province-level data
 sql = """CREATE TABLE data_pph_province (
    id INT AUTO_INCREMENT PRIMARY KEY,
    province VARCHAR(255),
    email VARCHAR(255),
    name VARCHAR(255),
    no_telp VARCHAR(255),
-   pendampingan_pelaku_usaha TEXT
+   kabupaten VARCHAR(255),   -- Adding kabupaten (regency)
+   kecamatan VARCHAR(255),   -- Adding kecamatan (sub-district)
+   no_registrasi VARCHAR(20),  -- Adding no_registrasi (registration number)
+   tgl_terbit DATE,   -- Adding tgl_terbit (issuance date)
+   lembaga VARCHAR(255),  -- Adding lembaga (institution)
+   pendampingan_pelaku_usaha TEXT -- Keeping the mentoring data
 )"""
 cursor.execute(sql)
 
-print('table data_pph created')
+print('Table data_pph_province created')
